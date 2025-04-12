78038f0e95bb7ce5584086d241bafd95b28de37e
alhothaify <118592381+alhothaify@users.noreply.github.com>
2025-04-12 21:34:35 +0300

    Display API categories on HomePage.

--- lib/features/home/presentation/pages/Home/widgets/categories.dart
+++ lib/features/home/presentation/pages/Home/widgets/categories.dart
@@ -1,44 +1,69 @@
 import 'package:flutter/material.dart';
-import '../models/category.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smartstore/features/categories/presentation/blocs/category_cubit.dart';
+import 'package:smartstore/features/categories/presentation/blocs/category_state.dart';
 
 class Categories extends StatelessWidget {
-  const Categories({
-    super.key,
-  });
+  const Categories({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 90,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        reverse: true, // عكس ترتيب العناصر
-        itemCount: categories.length,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Container(
-                height: 60,
-                width: 60,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage(categories[index].image),
-                    fit: BoxFit.cover, // التأكد من أن الصورة تملأ المساحة
-                  ),
-                ),
+      child: BlocBuilder<CategoryCubit, CategoryState>(
+        builder: (context, state) {
+          if (state is CategoryLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is CategoryLoaded) {
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              reverse: true,
+              itemCount: state.categories.length,
+              itemBuilder: (context, index) {
+                final category = state.categories[index];
+
+                return Column(
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: category.image != null && category.image!.isNotEmpty
+                            ? DecorationImage(
+                          image: NetworkImage(category.image!),
+                          fit: BoxFit.cover,
+                        )
+                            : null,
+                        color: Colors.grey[300], // لون خلفي عند غياب الصورة
+                      ),
+                      child: category.image == null || category.image!.isEmpty
+                          ? const Icon(Icons.image_not_supported)
+                          : null,
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      category.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
+            );
+          } else if (state is LoadCategoryFailure) {
+            return Center(
+              child: Text(
+                'خطأ: ${state.errorMessage}',
+                style: const TextStyle(color: Colors.red),
               ),
-              const SizedBox(height: 5),
-              Text(
-                categories[index].title,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          );
+            );
+          } else {
+            return const Center(child: Text('لا توجد بيانات متاحة'));
+          }
         },
-        separatorBuilder: (context, index) => const SizedBox(width: 20), // تعديل المسافة هنا
       ),
     );
   }
